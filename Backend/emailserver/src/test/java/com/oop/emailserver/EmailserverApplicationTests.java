19f152e10f0dcb459739970d090f3df74ad8d081
Saifullah-1 <siafmosad@gmail.com>
2023-12-12 15:51:30 +0200

    initial

---
+++ Backend/emailserver/src/test/java/com/oop/emailserver/EmailserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop.emailserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
