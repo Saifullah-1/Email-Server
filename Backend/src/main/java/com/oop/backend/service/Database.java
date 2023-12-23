c4caac1e83bf405ab749a33fa4059e928b0cede9
Saifullah-1 <siafmosad@gmail.com>
2023-12-23 22:02:08 +0200

    initial

--- Backend/src/main/java/com/oop/backend/service/Database.java
+++ Backend/src/main/java/com/oop/backend/service/Database.java
@@ -1,4 +1,10 @@
 package com.oop.backend.service;
 
+import com.oop.backend.module.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+    private List<User> users = new ArrayList<>();
 }
